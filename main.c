583a11a39472eaa6b1331fb6dbd6a0adbc172028
Alor79 <aallii.tmnt@gmail.com>
2020-06-01 23:06:50 +0100

    adding extra randomeness

--- main.c
+++ main.c
@@ -6,8 +6,9 @@
 //randomiseArray should randomly mix the order of elements in an array
 void randomiseArray(int arraySize, int arr[])
 {
+    static int Randomer=0;
     //giving rand a seed using time
-    srand(time(NULL));
+    srand(time(NULL)+Randomer++);
     //we swap every value with a random index(it can be it self)
     for (size_t i = 0; i < arraySize; i++)
     {
@@ -27,52 +28,74 @@ void printArray(int arraySize, int arr[])
     {
         printf("%d\t", arr[i]);
     }
-    puts("");puts("");
+
+}
+void newline(size_t x){
+    for (size_t i = 0; i < x; ++i) {
+        puts("");
+    }
 }
 int main() {
 
     int numbers[]={13,7,8,75,6,2,48,2,36,4,5,6,7,87,9};
     int n=sizeof(numbers)/ sizeof(numbers[0]);
-    puts("");
+    newline(1);
     puts("intro sort:");
     randomiseArray(n,numbers);
+    printArray(n,numbers);
+    newline(1);
     introSort(numbers,n);
     printArray(n,numbers);
+    newline(2);
 
-    puts("");
     puts("bubble sort:");
     randomiseArray(n,numbers);
+    printArray(n,numbers);
+    newline(1);
     bubbleSort(numbers,n);
     printArray(n,numbers);
+    newline(2);
 
-    puts("");
     puts("quick sort:");
     randomiseArray(n,numbers);
+    printArray(n,numbers);
+    newline(1);
     quickSort(numbers,n);
     printArray(n,numbers);
+    newline(2);
 
-    puts("");
-    puts("merge sort:(Bottom->Up)");
+    puts("merge sort:(Bottom->Up):");
     randomiseArray(n,numbers);
+    printArray(n,numbers);
+    newline(1);
     mergeSort(numbers,n);
     printArray(n,numbers);
+    newline(2);
 
-    puts("");
     puts("merge sort(Top->Down):");
     randomiseArray(n,numbers);
+    printArray(n,numbers);
+    newline(1);
     TopDownMergeSort(numbers,n);
     printArray(n,numbers);
+    newline(2);
 
-    puts("");
     puts("heap sort:");
     randomiseArray(n,numbers);
+    printArray(n,numbers);
+    newline(1);
     heapSort(numbers,n);
     printArray(n,numbers);
+    newline(2);
 
-    puts("");
     puts("insertion sort:");
     randomiseArray(n,numbers);
+    printArray(n,numbers);
+    newline(1);
     insertionSort(numbers,n);
     printArray(n,numbers);
+    newline(2);
+
+
     return 0;
 }
